52d7ac7e05cd7a1dd003482b1d47b693f38ae268
Carol <ikkilink@hotmail.com>
2026-03-26 10:39:17 -0600

    Menu

---
+++ Assets/Scripts/Ui/Paralexeffect.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Paralexeffect : MonoBehaviour
+{
+    Material material;
+    private float distance;
+
+    [Range(0f,0.5f)]
+    public float speed;
+
+    
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance += Time.deltaTime * speed;
+        material.SetTextureOffset("_MainTex", Vector2.right * distance);
+    }
+}
