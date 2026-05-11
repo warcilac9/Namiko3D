3ed38df3f1f97fd3bbc7591745c0c1faa052d3b5
Wilbert <warcilac9@gmail.com>
2026-05-11 17:32:25 -0600

    d

--- Assets/Scripts/Enemy/StateDebugger.cs
+++ Assets/Scripts/Enemy/StateDebugger.cs
@@ -5,7 +5,6 @@ public class StateDebugger : MonoBehaviour
 {
     private AI aiScript;
     public TMP_Text text;
-    public TMP_Text cooldown;
 
     void Start()
     {
@@ -15,6 +14,5 @@ public class StateDebugger : MonoBehaviour
     void Update()
     {
         text.text = aiScript.cState;
-        cooldown.text = 
     }
 }
